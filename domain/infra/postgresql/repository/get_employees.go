1693ccc22eca3c507c2f518c127511555f0c69a8
Andrew Silva <andrew.agoravai@gmail.com>
2024-08-24 21:20:04 -0300

    feat: implement PostgreSQL repository to get employees from db

---
+++ domain/infra/postgresql/repository/get_employees.go
@@ -0,0 +1,50 @@
+package repository
+
+import (
+	"context"
+	"tt-go-sample-api/domain/entity"
+	db "tt-go-sample-api/external/rdb/sqlc"
+)
+
+// GetEmployeesPostgreSQLRepository is a repository
+// used to retrieve many employees from the database.
+//
+// It implements the domain.GetEmployeesRepository
+// interface, and use PostgreSQL implementation
+// underneath.
+type GetEmployeesPostgreSQLRepository struct {
+}
+
+// NewGetEmployeesPostgreSQLRepository returns a pointer
+// to GetEmployeesPostgreSQLRepository.
+func NewGetEmployeesPostgreSQLRepository() *GetEmployeesPostgreSQLRepository {
+	return &GetEmployeesPostgreSQLRepository{}
+}
+
+// ListEmployees returns a list of employees from the
+// PostgreSQL database, paginated with page and limit
+// parameters.
+func (r *GetEmployeesPostgreSQLRepository) ListEmployees(ctx context.Context, page, limit int32) ([]*entity.Employee, error) {
+	employees, err := db.SQLStoreSingleton.ListEmployees(ctx, db.ListEmployeesParams{
+		Limit:  limit,
+		Offset: (page - 1) * limit,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	employeeEntities := make([]*entity.Employee, 0, len(employees))
+
+	for _, employee := range employees {
+		employeeEntities = append(employeeEntities, &entity.Employee{
+			ID:        employee.ID,
+			PublicID:  employee.PublicId.String(),
+			Name:      employee.Name,
+			CreatedAt: employee.CreatedAt,
+			UpdatedAt: employee.UpdatedAt,
+		})
+	}
+
+	return employeeEntities, nil
+}
