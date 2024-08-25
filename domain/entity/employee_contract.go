46122a488cb23836ea83d18c8d6baaad4bf5549f
Andrew Silva <andrew.agoravai@gmail.com>
2024-08-24 22:01:50 -0300

    feat: define interface to write employees in the database

--- domain/entity/employee_contract.go
+++ domain/entity/employee_contract.go
@@ -10,3 +10,11 @@ type GetEmployeesRepository interface {
 	// based on a page and a limit, used for pagination.
 	ListEmployees(ctx context.Context, page, limit int32) ([]*Employee, error)
 }
+
+// WriteEmployeeRepository defines the base methods
+// that any repository built to write or update employees
+// to/in a data store should use.
+type WriteEmployeeRepository interface {
+	// Register persists an employee in the data store.
+	Register(ctx context.Context, employee *Employee) (*Employee, error)
+}
